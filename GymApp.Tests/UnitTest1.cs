91d7cb00887476a1249ab1d453d5f5a50de8d2b2
Greta Pardyka <pardykag@gmail.com>
2023-11-18 13:35:53 +0100

    Testy

--- GymApp.Tests/UnitTest1.cs
+++ GymApp.Tests/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace GymApp.Tests
     public class Tests
     {
         [Test]
-        public void CheckSumOfPoints()
+        public void CheckIfStatisticsAreCorrectSumMaxMinFinalGrade()
         {
             //arrange
             var participant = new ParticipantInMemory("Scooby", "Doo");
@@ -20,59 +20,9 @@ namespace GymApp.Tests
             
             //assert
             Assert.AreEqual(59, statistics.Sum);
-        }
-
-        [Test]
-        public void CheckMaxPoint()
-        {
-            //arrange
-            var participant = new ParticipantInMemory("Scooby", "Doo");
-            participant.AddPoint(14);
-            participant.AddPoint(15);
-            participant.AddPoint(19);
-            participant.AddPoint(9);
-            participant.AddPoint(10);
-
-            //act
-            var statistics = participant.GetStatistics();
-
-            //assert
-            Assert.AreEqual(19, statistics.Max);
-        }
-
-        [Test]
-        public void CheckMinGrade()
-        {
-            //arrange
-            var participant = new ParticipantInMemory("Scooby", "Doo");
-            participant.AddPoint(14);
-            participant.AddPoint(15);
-            participant.AddPoint(12);
-            participant.AddPoint(2);
-            participant.AddPoint(9);
-
-            //act
-            var statistics = participant.GetStatistics();
-
-            //assert
-            Assert.AreEqual(2, statistics.Min);
-        }
-
-        [Test]
-        public void CheckFinalGrade()
-        {
-            //arrange
-            var participant = new ParticipantInMemory("Scooby", "Doo");
-            participant.AddPoint(20);
-            participant.AddPoint(17);
-            participant.AddPoint(19);
-            participant.AddPoint(15);
-            participant.AddPoint(16);
-            //act
-            var statistics = participant.GetStatistics();
-
-            //assert
-            Assert.AreEqual('5', statistics.FinalGrade);
+            Assert.AreEqual(20, statistics.Max);
+            Assert.AreEqual(1, statistics.Min);
+            Assert.AreEqual('3', statistics.FinalGrade);
         }
     }
 }
